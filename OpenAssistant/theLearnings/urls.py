7fbb522a869cb7770d416d6e61b13b64a7d197c9
Shivam Shukla <shivamshukla3108@gmail.com>
2024-04-20 20:16:42 +0530

    theLearning's Writing Section | Running...

--- OpenAssistant/theLearnings/urls.py
+++ OpenAssistant/theLearnings/urls.py
@@ -19,6 +19,8 @@ urlpatterns=[
 		# path('open/',views.open,name='open'),
 		# path('open/',views.open,name='open'),
 		# path('open/',views.open,name='open'),
+		# path('open/',views.open,name='open'),
+		# path('open/',views.open,name='open'),
 
 ]
 
