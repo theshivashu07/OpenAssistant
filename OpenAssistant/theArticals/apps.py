2f15ac0769325da157f3615e6fd968e54db42b2b
Shivam Shukla <shivamshukla3108@gmail.com>
2024-09-26 23:28:46 +0530

    Working on theLearning | Added shortcuts features ...

--- OpenAssistant/theArticals/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ThearticalsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'theArticals'
--- OpenAssistant/theLearnings/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ThelearningsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'theLearnings'
