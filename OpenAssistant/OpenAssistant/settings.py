68d2ed777bc558d184cb1ad690aa62ece1862ee4
Shivam Shukla <shivamshukla3108@gmail.com>
2025-01-28 23:25:42 +0530

    Working on theLearning | Added shortcuts features ...

--- OpenAssistant/OpenAssistant/settings.py
+++ OpenAssistant/OpenAssistant/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q-7wqv3=-(^9tu6%*trzw!*qwdwlnhiug)j1zh^mj(ija77=!#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
